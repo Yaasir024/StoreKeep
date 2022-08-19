de593d26474e379cd7acf0573cafd72614fb4437
Yaasir024 <yope4ever@gmail.com>
2022-08-19 21:01:28 +0100

    First Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="p-4 lg:px-140px lg:py-15px md:px-70px transition-all ease-in-out duration-700 border-b-2 md:border-none" >
+        <div class="flex items-center justify-between">
+            <div class="logo">
+                STOREKEEP
+            </div>
+            <div class="links flex">
+                <span>HOME</span>
+                <span class="ml-4">BLAH</span>
+                <span class="ml-4">PLANS</span>
+                <span class="ml-4">FAQs</span>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "sk-blue-100": "#00004d",
+        "sk-blue-200": "#002366",
+        "sk-blue-300": "#192bc2",
+        "sk-blue-400": "#0000ff",
+        "sk-blue-500": "#1560BD",
+        "sk-blue-600": "#73c2fb",
+        "sk-blue-700": "#1E90FF",
+        "sk-blue-800": "#87ceeb",
+      },
+    },
+  },
+  plugins: [],
+};
