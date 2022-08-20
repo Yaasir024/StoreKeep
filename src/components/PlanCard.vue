28a555c47ffc8e7197df6e5dcad1498f851871af
Yaasir024 <yope4ever@gmail.com>
2022-08-20 15:28:37 +0100

    /card component

--- src/components/PlanCard.vue
+++ src/components/PlanCard.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, reactive } from "vue";
-import PlanCard from '../components/PlanCArd.vue'
 
 const plansDetails = reactive({
   basic: {
@@ -17,14 +16,9 @@ const plansDetails = reactive({
 });
 </script>
 
+
 <template>
-  <section class="plans text-center max-w-7xl mx-auto">
-    <h1 class="text-32px text-sk-dark-100 font-medium tracking-0.01 mt-3">
-      PRICING
-    </h1>
-    <div class="card-grid max-w-5xl mx-auto mt-20 mb-10 grid grid-cols-3 gap-8">
-        <PlanCard />
-      <div class="card text-center border-2 border-gray-100 pt-5 px-10">
+    <div class="card text-center border-2 border-gray-100 pt-5 px-10">
         <p class="text-sk-dark-100 text-lg font-medium">BASIC</p>
         <div class="text-sk-dark-100 text-25px mt-6">$12/month</div>
         <div class="text-sk-gray-100 text-lg mt-2 mb-8 max-w-205px mx-auto">
@@ -51,6 +45,4 @@ const plansDetails = reactive({
           <li>15 Products</li>
         </ul>
       </div>
-    </div>
-  </section>
 </template>
