7f7d0f71c144a1c0e52adc570d6c06f4f9981dd5
Yaasir024 <yope4ever@gmail.com>
2022-08-20 15:16:55 +0100

    /addes check icon

--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -4,14 +4,33 @@
       PRICING
     </h1>
     <div class="card-grid max-w-5xl mx-auto mt-20 mb-10 grid grid-cols-3 gap-8">
-        <div class="card text-center border-2 border-gray-100 pt-5 px-10">
-            <p class="text-sk-dark-100 text-lg font-medium">BASIC</p>
-            <div class="text-sk-dark-100 text-25px mt-6">$12/month</div>
-            <div class="text-sk-gray-100 text-lg mt-2 mb-8 max-w-205px mx-auto">Billed annually or $15 month-to-month</div>
-            <ul class="list text-left list-none">
-                <li></li>
-            </ul>
+      <div class="card text-center border-2 border-gray-100 pt-5 px-10">
+        <p class="text-sk-dark-100 text-lg font-medium">BASIC</p>
+        <div class="text-sk-dark-100 text-25px mt-6">$12/month</div>
+        <div class="text-sk-gray-100 text-lg mt-2 mb-8 max-w-205px mx-auto">
+          Billed annually or $15 month-to-month
         </div>
+        <ul class="list text-left list-none">
+          <li class="flex items-center">
+            <svg
+              class="text-sk-gray-100 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width="20"
+              height="20"
+              fill="currentColor"
+            >
+              <path
+                d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"
+              />
+            </svg>
+            1 Store
+          </li>
+          <li>1 Invoice Template</li>
+          <li>2 Collaborators</li>
+          <li>15 Products</li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
