1152523d6734ac02fa3d69089e42ed8ef5f89f3a
Yaasir024 <yope4ever@gmail.com>
2022-08-22 15:38:28 +0100

    /upgrade banner

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -6,7 +6,7 @@ const system = useSystemStore();
 </script>
 
 <template>
-  <div :class="{'dark' : system.darkMode}">
+  <div :class="{ dark: system.darkMode }">
     <section
       class="bg-white text-sk-text-dark text-lg min-h-screen dark:bg-ps-gray-700 dark:text-sk-white"
     >
@@ -128,9 +128,9 @@ const system = useSystemStore();
               </button>
             </div>
           </div>
-          <div class="">
+          <div class="flex flex-row flex-wrap">
             <div
-              class="h-[270px] w-[310px] bg-sk-white dark:bg-ps-gray-800 shadow-xl flex flex-col rounded overflow-hidden cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1"
+              class="h-[270px] w-[310px] mr-9 bg-sk-white dark:bg-ps-gray-800 shadow-2xl flex flex-col rounded overflow-hidden cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1"
             >
               <div class="img-wrapper h-48">
                 <img
@@ -142,9 +142,33 @@ const system = useSystemStore();
               <div class="flex flex-col grow justify-center items-start px-6">
                 <div class="">
                   <p class="text-xl font-medium">Store Falana</p>
-                  <p>Jan 27, 2022</p>
+                  <p class="text-sm">Jan 27, 2022</p>
                 </div>
               </div>
+            </div>
+            <div
+              class="h-[270px] w-[310px] px-2 bg-sk-white dark:bg-ps-gray-800 shadow-2xl text-center rounded overflow-hidden cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1 flex flex-col items-center justify-center"
+            >
+              <div class="flex items-center justify-center mb-4 text-sk-blue-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  width="34"
+                  height="34"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M576 136c0 22.09-17.91 40-40 40c-.248 0-.4551-.1266-.7031-.1305l-50.52 277.9C482 468.9 468.8 480 453.3 480H122.7c-15.46 0-28.72-11.06-31.48-26.27L40.71 175.9C40.46 175.9 40.25 176 39.1 176c-22.09 0-40-17.91-40-40S17.91 96 39.1 96s40 17.91 40 40c0 8.998-3.521 16.89-8.537 23.57l89.63 71.7c15.91 12.73 39.5 7.544 48.61-10.68l57.6-115.2C255.1 98.34 247.1 86.34 247.1 72C247.1 49.91 265.9 32 288 32s39.1 17.91 39.1 40c0 14.34-7.963 26.34-19.3 33.4l57.6 115.2c9.111 18.22 32.71 23.4 48.61 10.68l89.63-71.7C499.5 152.9 496 144.1 496 136C496 113.9 513.9 96 536 96S576 113.9 576 136z"
+                  />
+                </svg>
+              </div>
+              <p class="text-xl text-sk-blue-700 mb-1">UPGRADE YOUR ACCOUNT</p>
+              <p class="px-4">Upgrade your account to unlock more features.</p>
+              <button
+                class="mt-5 py-2 px-5 text-white bg-sk-blue-700 rounded-2xl transition-all duration-300 ease-in-out hover:-translate-y-1"
+              >
+                <span>Upgrade Now</span>
+              </button>
             </div>
           </div>
         </div>
