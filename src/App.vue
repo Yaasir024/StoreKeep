b18e0d06c3c0120ba1715c572d8d34c79437a371
Yaasir024 <yope4ever@gmail.com>
2022-08-22 15:08:15 +0100

    Dark Mode hack

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import Toggle from "./components/Toggle.vue";
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import Navbar from "./components/Navbar.vue";
   <section class="hidden">
     <Navbar/>
   </section>
-
+  <Toggle/>
   <RouterView />
 </template>
 
