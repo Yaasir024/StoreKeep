aa7867bc2716381c46373d51a549e98f4ebe0c79
Yaasir024 <yope4ever@gmail.com>
2022-08-25 13:58:47 +0100

    /top of side bar

--- src/components/SidebarAccounts.vue
+++ src/components/SidebarAccounts.vue
@@ -1,5 +1,19 @@
 <template>
+  <div class="">
     <div class="bg-green-600 h-full w-[220px]">
-        Sidebar Jun
+      <div class="">
+        <div
+          class="flex flex-col items-center max-w-[220px] px-7 py-6 mb-2 border-b dark:border-ps-gray-500"
+        >
+          <div
+            class="bg-red-500 flex items-center justify-center h-12 w-12 rounded-[50%] text-sk-white text-2xl font-bold mr-2"
+          >
+            Y
+          </div>
+          <div class="">Yaasir Falana</div>
+          <div class="">yope4ever@gmail.com</div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
