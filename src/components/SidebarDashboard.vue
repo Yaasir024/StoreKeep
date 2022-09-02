7ccd72c13aa35d6c5cd0e1d7b2f6d04f75d4f0c8
Yaasir024 <yope4ever@gmail.com>
2022-09-02 13:10:19 +0100

    sidebar

--- src/components/SidebarDashboard.vue
+++ src/components/SidebarDashboard.vue
@@ -9,38 +9,122 @@
         </div>
         <div class="h-full">
           <ul class="px-4 flex flex-col text-base">
-            <RouterLink to="/account/account-settings">
+            <RouterLink to="/store/home">
               <li
-                class="py-3 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
+                class="flex items-center py-3 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
               >
-                Home
+                <svg
+                  class="mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path
+                    d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+                  ></path>
+                  <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                </svg>
+                <span class="text-lg"> Home </span>
               </li>
             </RouterLink>
+            <RouterLink to="/store/products">
+              <li
+                class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800 border-t border-ps-gray-500"
+              >
+                <svg
+                  class="mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"
+                  />
+                </svg>
+                <span class="text-lg"> Products </span>
+              </li>
+            </RouterLink>
+            <li
+              class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
+            >
+              <svg
+                class="mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M6.665 9.068l-3.665-1.66v14l3.665 2.592 14.335-2.155v-14.845l-14.335 2.068zm-1.665 1.441l1 .453v10.118l-1-.707v-9.864zm14 9.615l-11 1.653v-10.881l11-1.587v10.815zm-2-15.833l-.001 1.749c0 .246-.18.455-.423.492-.303.045-.576-.19-.576-.495v-1.746c.001-.691-.231-1.304-.653-1.726-.368-.37-.847-.565-1.384-.565-1.547 0-2.96 1.558-2.963 3.268v1.681c0 .247-.181.457-.425.494-.302.046-.575-.189-.575-.494l.001-1.683c.004-2.261 1.866-4.266 3.962-4.266 1.717 0 3.039 1.387 3.037 3.291zm-9.999 2.209v-2.235c.004-2.26 1.866-4.265 3.962-4.265.492 0 .944.125 1.35.332-.423.17-.822.4-1.188.683l-.162-.015c-1.547 0-2.961 1.558-2.963 3.268v2.232c0 .248-.182.458-.427.494-.3.045-.572-.187-.572-.494z"
+                />
+              </svg>
+              <span class="text-lg"> Orders </span>
+            </li>
+            <li
+              class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
+            >
+              <svg
+                class="mr-2"
+                width="24"
+                height="24"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path
+                  d="M22 24h-20v-24h14l6 6v18zm-7-23h-12v22h18v-16h-6v-6zm3 15v1h-12v-1h12zm0-3v1h-12v-1h12zm0-3v1h-12v-1h12zm-2-4h4.586l-4.586-4.586v4.586z"
+                />
+              </svg>
 
-            <li
-              class="py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800 border-t border-ps-gray-500"
-            >
-              Products
+              <span class="text-lg"> Invoice </span>
             </li>
             <li
-              class="py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
+              class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
             >
-              Orders
+              <svg
+                class="mr-2"
+                width="25"
+                height="25"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path
+                  d="M5 11v1h8v-7h-10v-1c0-.552.448-1 1-1h10c.552 0 1 .448 1 1v2h4.667c1.117 0 1.6.576 1.936 1.107.594.94 1.536 2.432 2.109 3.378.188.312.288.67.288 1.035v4.48c0 1.089-.743 2-2 2h-1c0 1.656-1.344 3-3 3s-3-1.344-3-3h-4c0 1.656-1.344 3-3 3s-3-1.344-3-3h-1c-.552 0-1-.448-1-1v-6h-2v-2h7v2h-3zm3 5.8c.662 0 1.2.538 1.2 1.2 0 .662-.538 1.2-1.2 1.2-.662 0-1.2-.538-1.2-1.2 0-.662.538-1.2 1.2-1.2zm10 0c.662 0 1.2.538 1.2 1.2 0 .662-.538 1.2-1.2 1.2-.662 0-1.2-.538-1.2-1.2 0-.662.538-1.2 1.2-1.2zm-3-2.8h-10v2h.765c.549-.614 1.347-1 2.235-1 .888 0 1.686.386 2.235 1h5.53c.549-.614 1.347-1 2.235-1 .888 0 1.686.386 2.235 1h1.765v-4.575l-1.711-2.929c-.179-.307-.508-.496-.863-.496h-4.426v6zm1-5v3h5l-1.427-2.496c-.178-.312-.509-.504-.868-.504h-2.705zm-16-3h8v2h-8v-2z"
+                />
+              </svg>
+              <span class="text-lg"> Shipping </span>
             </li>
             <li
-              class="py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
+              class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800 border-b border-ps-gray-500"
             >
-              Invoice
-            </li>
-            <li
-              class="py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
-            >
-              Shipping
-            </li>
-            <li
-              class="py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800 border-b border-ps-gray-500"
-            >
-              Overview
+              <svg
+                class="mr-2"
+                viewBox="0 0 512 512"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                width="24"
+                height="24"
+              >
+                <path
+                  d="M76 240c12.1 0 23.1-4.8 31.2-12.6l44.2 22A44.9 44.9 0 00196 300a45 45 0 0040.6-64.4l60-60a45 45 0 0062.3-54l52.2-39.2a45 45 0 10-18-24l-52.2 39.2a45 45 0 00-65.5 56.8l-60 60a44.7 44.7 0 00-50.6 8.2l-44.2-22A44.9 44.9 0 0076 150a45 45 0 000 90zM436 30a15 15 0 110 30 15 15 0 010-30zm-120 90a15 15 0 110 30 15 15 0 010-30zM196 240a15 15 0 110 30 15 15 0 010-30zM76 180a15 15 0 110 30 15 15 0 010-30zm0 0"
+                ></path>
+                <path
+                  d="M497 482h-16V165a15 15 0 00-15-15h-60a15 15 0 00-15 15v317h-30V255a15 15 0 00-15-15h-60a15 15 0 00-15 15v227h-30V375a15 15 0 00-15-15h-60a15 15 0 00-15 15v107h-30V315a15 15 0 00-15-15H46a15 15 0 00-15 15v167H15a15 15 0 100 30h482a15 15 0 100-30zm-76-302h30v302h-30zm-120 90h30v212h-30zM181 390h30v92h-30zM61 330h30v152H61zm0 0"
+                ></path>
+              </svg>
+              <span class="text-lg"> Overview </span>
             </li>
             <li
               class="flex items-center py-2 cursor-pointer transition-all duration-300 ease-in-out hover:text-sk-blue-800"
